172cad82f4208c442682d227ba9a18d9c4d6a2f8
ardyh <13517062@std.stei.itb.ac.id>
2020-01-11 23:47:36 +0700

    added find all book api

---
+++ src/main/java/com/kindle/backend/response/attributeResponse/GetBookResponse.java
@@ -0,0 +1,94 @@
+package com.kindle.backend.response.attributeResponse;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetBookResponse {
+    private String title;
+    private String author;
+    private int year;
+    private String description;
+    private float price;
+    private String document;
+    private String variant;
+    private String url;
+    private String categories;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getVariant() {
+        return variant;
+    }
+
+    public void setVariant(String variant) {
+        this.variant = variant;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getCategories() {
+        return categories;
+    }
+
+    public void setCategories(String categories) {
+        this.categories = categories;
+    }
+}
--- src/main/java/com/kindle/backend/response/relationshipResponse/BaseRelationshipDataResponse.java
+++ src/main/java/com/kindle/backend/response/relationshipResponse/BaseRelationshipDataResponse.java
@@ -1,5 +1,6 @@
 package com.kindle.backend.response.relationshipResponse;
 
+import com.kindle.backend.response.dataResponse.DataNoAttributeResponse;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,14 +10,14 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class BaseRelationshipDataResponse<T> {
-  private List<T> data;
+public class BaseRelationshipDataResponse {
+  private List<DataNoAttributeResponse> data;
 
-  public List<T> getData() {
+  public List<DataNoAttributeResponse> getData() {
     return data;
   }
 
-  public void setData(List<T> data) {
+  public void setData(List<DataNoAttributeResponse> data) {
     this.data.addAll(data);
   }
 }
