7bb6e11ca68907b69a8f17f35af0378451db867f
ramadhanriandi <izurbudianto@gmail.com>
2020-01-11 20:05:45 +0700

    add relation response class

---
+++ src/main/java/com/kindle/backend/response/SuccessDataWithIncludedResponse.java
@@ -0,0 +1,49 @@
+package com.kindle.backend.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SuccessDataWithIncludedResponse<T1, T2> extends BaseResponse {
+  private int code;
+  private String status;
+  private List<T1> data;
+  private List<T2> included;
+
+  public int getCode() {
+    return code;
+  }
+  
+  public void setCode(int code) {
+    this.code = code;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public List<T1> getData() {
+    return data;
+  }
+
+  public void setData(List<T1> data) {
+    this.data.addAll(data);
+  }
+
+  public List<T2> getIncluded() {
+    return included;
+  }
+
+  public void setIncluded(List<T2> included) {
+    this.included.addAll(included);
+  }
+}
